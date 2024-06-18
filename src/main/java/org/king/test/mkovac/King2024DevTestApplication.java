aa2d96cf5feaa3b1620c9a0aa31c7a5a64d9a476
Marijan Kovač <mkovac@student.foi.hr>
2024-06-18 16:20:35 +0200

    Inicijalizacija Spring projekta

---
+++ src/main/java/org/king/test/mkovac/King2024DevTestApplication.java
@@ -0,0 +1,13 @@
+package org.king.test.mkovac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class King2024DevTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(King2024DevTestApplication.class, args);
+	}
+
+}
